66f1c9a16e0f2559d7cf49af39d8324276ab1f1c
jrrose <jrrose@uw.edu>
2022-01-16 17:01:56 -0800

    added lesson 2 calculator.js

---
+++ lesson02/calculator.js
@@ -0,0 +1,10 @@
+let five = 5;
+let eight = 8;
+let cow = "tipsy"
+let ten = 10;
+console.log(five * eight);
+console.log(five * cow);
+console.log(ten + cow);
+console.log(eight - ten);
+console.log(eight + cow + ten);
+console.log(ten + "ten");
