5b723373f84d3cf89708e02fcc118be898585b82
jrrose <jrrose@uw.edu>
2022-01-25 13:10:06 -0800

    Finished challenge3 of lesson3

--- lesson03/challenge3.js
+++ lesson03/challenge3.js
@@ -1,4 +1,4 @@
-const array = ["tipsy", "im cold", "spider-man", 22, "cedarwood"];
+const array = ["tipsy cow", "im cold", "spider-man", 22, "cedarwood"];
 for (let i = 0; i < array.length; i++) {
     let element = array[i];
     console.log(i + "," + element);
