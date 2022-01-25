25d4a827cbab714be52d7b5ac689c23df5494a7d
jrrose <jrrose@uw.edu>
2022-01-25 12:43:23 -0800

    finished challenge 3 of lesson 3

--- lesson03/challenge3.js
+++ lesson03/challenge3.js
@@ -1,5 +1,5 @@
 const array = ["tipsy", "im cold", "spider-man", 22, "cedarwood"];
 for (let i = 0; i < array.length; i++) {
-    const element = array[i];
+    let element = array[i];
     console.log(i + "," + element);
 }
